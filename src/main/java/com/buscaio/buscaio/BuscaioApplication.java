b3cff371cab145971b2cde7c5a6d79515678f9bb
rHelen <rutehelenrh@gmail.com>
2023-11-24 19:33:26 -0300

    Ajustando Controller e Swagger doc

---
+++ src/main/java/com/buscaio/buscaio/BuscaioApplication.java
@@ -0,0 +1,13 @@
+package com.buscaio.buscaio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscaioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscaioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buscaio/buscaio/BuscaioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buscaio.buscaio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscaioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
